f04ee71079b8db958bd52aa7f6924eaed01fa7ec
Eddy Nguyen <eddeee.nguyen@gmail.com>
2022-05-01 22:38:52 +1000

    Remove swc packages

--- packages/nx-graphql-code-generator/jest.config.ts
+++ packages/nx-graphql-code-generator/jest.config.ts
@@ -1,14 +1,13 @@
-const fs = require('fs');
-
-// Reading the SWC compilation config and remove the "exclude"
-// for the test files to be compiled by SWC
-const { exclude: _, ...swcJestConfig } = JSON.parse(fs.readFileSync(`${__dirname}/.lib.swcrc`, 'utf-8'));
-
 module.exports = {
   displayName: 'nx-graphql-code-generator',
   preset: '../../jest.preset.ts',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
   transform: {
-    '^.+\\.[tj]s$': ['@swc/jest', swcJestConfig],
+    '^.+\\.[tj]s$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../coverage/packages/nx-graphql-code-generator',
