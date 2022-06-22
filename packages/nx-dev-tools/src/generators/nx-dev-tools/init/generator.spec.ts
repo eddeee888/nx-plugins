0dcf6260ab5c2db694320ab3a5e3f247aa352605
Eddy Nguyen <ch@eddeee888.me>
2022-06-22 18:02:45 +1000

    Update templates
    - dc-down
    - dc-exec
    - dc-logs
    - dc-rebuild
    - dc-recreate
    - dc-run
    - dc-shell
    - dc-start
    - dc-stop
    - dc-up
    - init-cert
    - init
    - utils-constants
    - utils-dc
    - vm-down
    - vm-up
    - ws-nx

--- packages/nx-dev-tools/src/generators/nx-dev-tools/init/generator.spec.ts
+++ packages/nx-dev-tools/src/generators/nx-dev-tools/init/generator.spec.ts
@@ -14,12 +14,30 @@ describe('nx-dev-tools generator', () => {
   });
 
   describe('Files generation', () => {
-    test.each([['dev-tools/bin/core.sh'], ['dev-tools/bin/dc-build-dev-images.sh'], ['dev-tools/bin/dc-build.sh']])(
-      '%s content',
-      (file) => {
-        const content = tree.read(file, 'utf-8');
-        expect(content).toMatchSnapshot();
-      }
-    );
+    test.each([
+      ['dev-tools/bin/core.sh'],
+      ['dev-tools/bin/dc-build-dev-images.sh'],
+      ['dev-tools/bin/dc-build.sh'],
+      ['dev-tools/bin/dc-down.sh'],
+      ['dev-tools/bin/dc-exec.sh'],
+      ['dev-tools/bin/dc-logs.sh'],
+      ['dev-tools/bin/dc-rebuild.sh'],
+      ['dev-tools/bin/dc-recreate.sh'],
+      ['dev-tools/bin/dc-run.sh'],
+      ['dev-tools/bin/dc-shell.sh'],
+      ['dev-tools/bin/dc-start.sh'],
+      ['dev-tools/bin/dc-stop.sh'],
+      ['dev-tools/bin/dc-up.sh'],
+      ['dev-tools/bin/init-cert.sh'],
+      ['dev-tools/bin/init.sh'],
+      ['dev-tools/bin/utils-constants.sh'],
+      ['dev-tools/bin/utils-dc.sh'],
+      ['dev-tools/bin/vm-down.sh'],
+      ['dev-tools/bin/vm-up.sh'],
+      ['dev-tools/bin/vm-nx.sh'],
+    ])('%s content', (file) => {
+      const content = tree.read(file, 'utf-8');
+      expect(content).toMatchSnapshot();
+    });
   });
 });
