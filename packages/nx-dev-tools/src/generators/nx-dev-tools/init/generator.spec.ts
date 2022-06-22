e33feb65b5cd12ad352d6ac7e0d0807f2429359b
Eddy Nguyen <ch@eddeee888.me>
2022-06-22 17:45:45 +1000

    Add build-dev-images

--- packages/nx-dev-tools/src/generators/nx-dev-tools/init/generator.spec.ts
+++ packages/nx-dev-tools/src/generators/nx-dev-tools/init/generator.spec.ts
@@ -88,5 +88,37 @@ describe('nx-dev-tools generator', () => {
       main $@
       "
     `);
+
+    const buildDevImages = tree.read('dev-tools/bin/dc-build-dev-images.sh', 'utf-8');
+    expect(buildDevImages).toMatchInlineSnapshot(`
+      "#!/bin/bash
+
+      set -e
+
+      source $UTILS_CONST
+
+      function clean_images {
+        docker rmi -f $PROJECT_NAME/dev
+        docker rmi -f $PROJECT_NAME/dev-backend
+      }
+
+      function build_dev_images {
+        image=$1
+
+        docker-compose \\\\
+          --file dev-tools/docker-image/build-dev-image.yml \\\\
+          --env-file=.env.docker-compose \\\\
+          build --no-cache $image
+      }
+
+      function main {
+        clean_images
+        build_dev_images dev
+        build_dev_images dev-backend
+      }
+
+      main
+      "
+    `);
   });
 });
