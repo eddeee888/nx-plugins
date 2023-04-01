11b70440b030257d48f9397933417e9a85943779
Eddy Nguyen <ch@eddeee888.me>
2023-04-01 22:14:29 +1100

    Apply migration changes

--- jest.preset.js
+++ jest.preset.js
@@ -1,3 +1,6 @@
 const nxPreset = require('@nrwl/jest/preset').default;
 
-module.exports = { ...nxPreset, testEnvironment: 'node' };
+module.exports = {
+  ...nxPreset,
+  testEnvironment: 'node',
+};
