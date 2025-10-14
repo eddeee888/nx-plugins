51cf85d1a043bf9a7b9bd45555239cbf3b4e448b
Eddy Nguyen <ch@eddeee888.me>
2025-10-14 22:20:03 +1100

    Convert to flat config (#138)

---
+++ eslint.config.cjs
@@ -0,0 +1,35 @@
+const nx = require('@nx/eslint-plugin');
+
+module.exports = [
+  ...nx.configs['flat/base'],
+  ...nx.configs['flat/typescript'],
+  ...nx.configs['flat/javascript'],
+  {
+    ignores: ['**/dist'],
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    rules: {
+      '@nx/enforce-module-boundaries': [
+        'error',
+        {
+          enforceBuildableLibDependency: true,
+          allow: [],
+          depConstraints: [
+            {
+              sourceTag: '*',
+              onlyDependOnLibsWithTags: ['*'],
+            },
+          ],
+        },
+      ],
+    },
+  },
+  {
+    files: ['**/*.json'],
+    languageOptions: {
+      parser: require('jsonc-eslint-parser'),
+    },
+    rules: {},
+  },
+];
