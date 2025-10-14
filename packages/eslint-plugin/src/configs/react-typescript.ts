570bb4482cecbd0166a0b3858f262a13e6e342ff
Eddy Nguyen <github@eddeee888.me>
2025-10-14 23:58:10 +1100

    Convert to eslint-plugin to flat config

--- packages/eslint-plugin/src/configs/react-typescript.ts
+++ packages/eslint-plugin/src/configs/react-typescript.ts
@@ -1,8 +1,27 @@
-export const reactTypescriptConfig = {
-  settings: { react: { version: 'detect' } },
-  plugins: ['jsx-a11y', 'react'],
-  extends: ['plugin:react-hooks/recommended', 'plugin:jsx-a11y/recommended'],
-  rules: {
-    'react/jsx-no-useless-fragment': ['error', { allowExpressions: true }],
+import { defineConfig } from 'eslint/config';
+import * as jsxA11yPlugin from 'eslint-plugin-jsx-a11y';
+import * as reactPlugin from 'eslint-plugin-react';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
+
+export const reactTypescriptConfig = defineConfig(
+  {
+    files: ['**/*.ts', '**/*.cts', '**/*.mts', '**/*.tsx', '**/*.js', '**/*.cjs', '**/*.mjs', '**/*.jsx'],
+    plugins: {
+      'react-hooks': reactHooksPlugin,
+    },
+    rules: reactHooksPlugin.configs.recommended.rules,
   },
-};
+  {
+    files: ['**/*.ts', '**/*.cts', '**/*.mts', '**/*.tsx', '**/*.js', '**/*.cjs', '**/*.mjs', '**/*.jsx'],
+    settings: { react: { version: 'detect' } },
+    plugins: {
+      'jsx-a11y': jsxA11yPlugin,
+      react: reactPlugin,
+    },
+    rules: {
+      ...jsxA11yPlugin.flatConfigs.recommended.rules,
+      ...reactPlugin.configs.recommended.rules,
+      'react/jsx-no-useless-fragment': ['error', { allowExpressions: true }],
+    },
+  }
+);
--- packages/eslint-plugin/src/configs/typescript.ts
+++ packages/eslint-plugin/src/configs/typescript.ts
@@ -1,32 +1,41 @@
-export const typescriptConfig = {
-  plugins: ['@typescript-eslint'],
-  parser: '@typescript-eslint/parser',
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-  ],
-  rules: {
-    '@typescript-eslint/array-type': 'error',
-    '@typescript-eslint/await-thenable': 'error',
-    '@typescript-eslint/consistent-type-exports': ['error', { fixMixedExportsWithInlineTypeSpecifier: true }],
-    '@typescript-eslint/consistent-type-imports': ['error', { fixStyle: 'inline-type-imports' }],
-    '@typescript-eslint/explicit-function-return-type': [
-      'error',
-      { allowExpressions: true, allowTypedFunctionExpressions: true },
-    ],
-    '@typescript-eslint/no-confusing-non-null-assertion': 'error',
-    '@typescript-eslint/no-duplicate-type-constituents': 'error',
-    '@typescript-eslint/no-floating-promises': 'error',
-    '@typescript-eslint/no-import-type-side-effects': 'error',
-    '@typescript-eslint/no-non-null-assertion': 'error',
-    'no-shadow': 'off',
-    '@typescript-eslint/no-shadow': 'error',
-    'no-throw-literal': 'off',
-    '@typescript-eslint/only-throw-error': 'error',
-    '@typescript-eslint/no-unsafe-member-access': 'error',
-    'no-unused-expressions': 'off',
-    '@typescript-eslint/no-unused-expressions': 'error',
-    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+import * as eslint from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
+
+export const typescriptConfig = defineConfig(
+  {
+    plugins: { '@typescript-eslint': tseslint.plugin },
+    languageOptions: {
+      parser: tseslint.parser,
+    },
   },
-};
+  eslint.configs.recommended,
+  tseslint.configs.eslintRecommended,
+  tseslint.configs.recommended,
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.cts', '**/*.mts'],
+    rules: {
+      '@typescript-eslint/array-type': 'error',
+      '@typescript-eslint/await-thenable': 'error',
+      '@typescript-eslint/consistent-type-exports': ['error', { fixMixedExportsWithInlineTypeSpecifier: true }],
+      '@typescript-eslint/consistent-type-imports': ['error', { fixStyle: 'inline-type-imports' }],
+      '@typescript-eslint/explicit-function-return-type': [
+        'error',
+        { allowExpressions: true, allowTypedFunctionExpressions: true },
+      ],
+      '@typescript-eslint/no-confusing-non-null-assertion': 'error',
+      '@typescript-eslint/no-duplicate-type-constituents': 'error',
+      '@typescript-eslint/no-floating-promises': 'error',
+      '@typescript-eslint/no-import-type-side-effects': 'error',
+      '@typescript-eslint/no-non-null-assertion': 'error',
+      'no-shadow': 'off',
+      '@typescript-eslint/no-shadow': 'error',
+      'no-throw-literal': 'off',
+      '@typescript-eslint/only-throw-error': 'error',
+      '@typescript-eslint/no-unsafe-member-access': 'error',
+      'no-unused-expressions': 'off',
+      '@typescript-eslint/no-unused-expressions': 'error',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+    },
+  }
+);
