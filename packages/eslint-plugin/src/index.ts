14bab607c8900bece61ff7ba8625fed7679cab78
Eddy Nguyen <github@eddeee888.me>
2025-10-15 08:44:55 +1100

    Fix export

--- packages/eslint-plugin/src/index.ts
+++ packages/eslint-plugin/src/index.ts
@@ -2,11 +2,11 @@ import { baseTypescriptConfig } from './configs/base-typescript';
 import { typescriptConfig } from './configs/typescript';
 import { reactTypescriptConfig } from './configs/react-typescript';
 
-export default {
-  configs: {
-    ['base-typescript']: baseTypescriptConfig,
-    typescript: typescriptConfig,
-    ['react-typescript']: reactTypescriptConfig,
-  },
-  rules: {},
+export const configs = {
+  ['base-typescript']: baseTypescriptConfig,
+  typescript: typescriptConfig,
+  ['react-typescript']: reactTypescriptConfig,
 };
+export const rules = {};
+
+export default { configs, rules };
