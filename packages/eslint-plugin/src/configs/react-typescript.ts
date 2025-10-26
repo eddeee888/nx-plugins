71b8b6bddfe471af46a565383ed20f4b66332296
Eddy Nguyen <ch@eddeee888.me>
2025-10-26 15:22:39 +1100

    Add changeset (#143)

--- packages/eslint-plugin/src/configs/react-typescript.ts
+++ packages/eslint-plugin/src/configs/react-typescript.ts
@@ -3,10 +3,12 @@ import * as jsxA11yPlugin from 'eslint-plugin-jsx-a11y';
 import * as reactPlugin from 'eslint-plugin-react';
 const reactHooksPlugin = require('eslint-plugin-react-hooks'); // We must use require() here because react-hooks is cjs, and import will use `default` instead of the cjs module.exports
 
+const filePatterns = ['**/*.ts', '**/*.cts', '**/*.mts', '**/*.tsx', '**/*.js', '**/*.cjs', '**/*.mjs', '**/*.jsx'];
+
 export const reactTypescriptConfig = defineConfig(
   {
     name: '@eddeee888/eslint-plugin/react-typescript/hooks',
-    files: ['**/*.ts', '**/*.cts', '**/*.mts', '**/*.tsx', '**/*.js', '**/*.cjs', '**/*.mjs', '**/*.jsx'],
+    files: filePatterns,
     plugins: {
       'react-hooks': reactHooksPlugin,
     },
@@ -14,7 +16,7 @@ export const reactTypescriptConfig = defineConfig(
   },
   {
     name: '@eddeee888/eslint-plugin/react-typescript/react',
-    files: ['**/*.ts', '**/*.cts', '**/*.mts', '**/*.tsx', '**/*.js', '**/*.cjs', '**/*.mjs', '**/*.jsx'],
+    files: filePatterns,
     settings: { react: { version: 'detect' } },
     plugins: {
       'jsx-a11y': jsxA11yPlugin,
@@ -23,6 +25,7 @@ export const reactTypescriptConfig = defineConfig(
     rules: {
       ...jsxA11yPlugin.flatConfigs.recommended.rules,
       ...reactPlugin.configs.recommended.rules,
+      'react/react-in-jsx-scope': 'off', // This is used for legacy React apps where `import React` must be explicit. Modern apps do not need this import.
       'react/jsx-no-useless-fragment': ['error', { allowExpressions: true }],
     },
   }
